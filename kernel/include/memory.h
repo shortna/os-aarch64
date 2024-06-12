0941c240c9f4789a0ca85b31451fde050707148b
Dmytro Katunin <katunin_git@proton.me>
2024-06-12 14:00:56 +0300

    leave it at there

--- kernel/include/memory.h
+++ kernel/include/memory.h
@@ -5,7 +5,16 @@
 
 #define ALLOC_MAX U16(1024) // 1Kb
 
-extern void *memalloc(u32 size);
-extern void memfree(void *p);
+extern void *mem_alloc(u16 size);
+extern void *mem_realloc(void *p, u16 size);
+extern void mem_free(void *p);
+
+/*
+ * Shifts `n` bytes from `p` which has size of `size`.
+ */
+extern void mem_shift_right(void *p, u16 size, u16 n);
+extern void mem_shift_left(void *p, u16 size, u16 n);
+
+extern void mem_strap(void);
 
 #endif
--- kernel/src/k.c
+++ kernel/src/k.c
@@ -1,3 +1,35 @@
+#include "memory.h"
+
 void kmain() {
+  mem_strap();
+  u8 **m = mem_alloc(UINT8_MAX * sizeof(*m));
+  for (u8 i = 0; i < UINT8_MAX; i++) {
+    m[i] = mem_alloc(UINT8_MAX * sizeof(*m[i]));
+    for (u8 j = 0; j < UINT8_MAX; j++) {
+      m[i][j] = j;
+    }
+  }
+
+  u8 *a = mem_alloc(sizeof(*a));
+  u16 *b = mem_alloc(sizeof(*b));
+  u8 *c = mem_alloc(sizeof(*c));
+
+  *a = 'M';
+  *b = 'E';
+  *(b + 1) = 'E';
+  *c = 'M';
+
+  mem_free(b);
+  u8 *d = mem_alloc(sizeof(*d));
+  u8 *e = mem_alloc(sizeof(*e));
+
+  *d = 'N';
+  *e = 'N';
+
+  for (u8 i = 0; i < UINT8_MAX; i++) {
+    mem_free(m[i]);
+  }
+  mem_free(m);
+
   while (1);
 }
