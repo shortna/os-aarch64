6e0c7801fdfabebcb11e1568d04e1fe1dd427aa0
Dmytro Katunin <katunin_git@proton.me>
2024-06-30 10:26:40 +0300

    changes to main that needed by previous commits

--- kernel/src/k.c
+++ kernel/src/k.c
@@ -1,24 +1,25 @@
-//#include "uart/uart.h"
-//#include "gpio/gpio.h"
+#include "uart/uart.h"
 #include "rtc/rtc.h"
 #include "gic/gic.h"
 
-#define GPIO_BASE_ADDRESS U64(0x9030000)
+// 24MHz
+#define APB_CLOCK U64(24000000)
 
 #define RTC_BASE_ADDRESS U64(0x9010000)
 
-#define UART_CLOCK U64(0x16e3600)
+#define UART_CLOCK APB_CLOCK
 #define UART_BAUD_RATE U64(115200)
 #define UART_BASE_ADDRESS U64(0x9000000)
 
 #define GICD_BASE_ADDRESS U64(0x8000000)
 #define GICR_BASE_ADDRESS U64(0x80a0000)
 
-extern u32 timer_physical_time_passed(void);
-extern u64 timer_physical_value(void);
+extern u64 timer_system_get_frequency(void);
+extern u64 timer_physical_get_value(void);
 extern void timer_physical_enable(void);
-extern void timer_physical_absolute_threshold(u64 threshold);
-extern void timer_physical_add_threshold(u32 v);
+extern void timer_physical_set_threshold(u64 t);
+extern void timer_physical_add_threshold(u32 t);
+extern u32 timer_physical_time_passed(void);
 
 void kmain() {
   GICD dist = gicd(GICD_BASE_ADDRESS);
@@ -62,28 +63,25 @@ void kmain() {
   int_enable(dist, redists, pl031_int, redistributor_id);
 
   RTC r = rtc(RTC_BASE_ADDRESS);
-  rtc_add_offset(r, 0);
-  rtc_set_match(r, MINUTE);
+  rtc_reset(r);
+  rtc_set_match(r, rtc_get_current(r) + MINUTE);
   rtc_enable_interrupt(r);
-  rtc_enable(r);
 */
 
-/*
   // pl011 interrupts
   // uart_interrupts = <0x00 0x01 0x04>;
-  u8 uart_int = 33;
-  int_set_priority(uart_int, redistributor_id, 0);                    // GIC(D|R)_IPRIORITYRn
-  int_set_group(uart_int, redistributor_id, GICV3_GROUP1_NON_SECURE); // GIC(D|R)_IGROUPn GIC(D|R)_IGRPMODn
-  int_set_triger(uart_int, redistributor_id, GICV3_LEVEL_SENSITIVE);  // GIC(D|R)_ICFGRn
-  int_set_route(uart_int, getAffinity());                             // GICD_IROUTERn (only for SPI`s)
-  int_enable(uart_int, redistributor_id);                             // GIC(D|R)_ISENABLERn
+  u8 pl011_int = 33;
+  int_set_priority(dist, redists, pl011_int, redistributor_id, 0);
+  int_set_group(dist, redists, pl011_int, redistributor_id, GICV3_GROUP1_NON_SECURE);
+  int_set_triger(dist, redists, pl011_int, redistributor_id, GICV3_LEVEL_SENSITIVE);
+  int_set_route(dist, pl011_int, getAffinity());
+  int_enable(dist, redists, pl011_int, redistributor_id);
 
-  Uart *uart0 = uart_setup(UART_BASE_ADDRESS, UART_CLOCK, UART_BAUD_RATE);
+  Uart uart0 = uart_setup(UART_BASE_ADDRESS, UART_CLOCK, UART_BAUD_RATE);
 
   for (u8 i = 'A'; i < 'z'; i++) {
     uart_write_byte(uart0, i);
   }
-*/
 
   while (1);
 }
