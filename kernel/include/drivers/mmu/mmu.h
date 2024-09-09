5a182e1966227301efa36b8907f02341503b6680
Katunin Dmytro <Katunin_git@proton.me>
2024-09-09 12:49:23 +0300

    add funciton to convert address to n pages for malloc

--- kernel/include/drivers/mmu/mmu.h
+++ kernel/include/drivers/mmu/mmu.h
@@ -3,6 +3,7 @@
 
 #include "types.h"
 
+#define _1GB                 ((uint64_t)1024 * 1024 * 1024)
 enum GRANULARITY_TYPE {
   GRANULARITY_16KB = 0x1,
   GRANULARITY_4KB = 0x2,
@@ -15,5 +16,9 @@ extern bool mmu_enable(void);
 extern uint64_t PAGE_SIZE;
 extern void* malloc(uint16_t n_blocks);
 extern void free(void *addr);
+
+static inline uint16_t size_to_pages(uint32_t size) {
+  return size / PAGE_SIZE + (bool)(size % PAGE_SIZE);
+}
 
 #endif
